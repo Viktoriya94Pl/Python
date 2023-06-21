52ccd131b1944c4908df6992257c9204d8f1c57f
Vika Platonova <vplatonova_1994@mail.ru>
2023-06-21 23:33:46 +0300

    Homework 2

--- 2/main.py
+++ 2/main.py
@@ -1,16 +1,21 @@
 
-# number = int(input("Enter the number of coins: "))
-# count = 0
-# for i in range(number):
-#     coins = int(input())
-#     if coins <= 0:
-#         count += 1
-# print(count)
+number = int(input("Enter the number of coins: "))
+count = 0
+for i in range(number):
+    coins = int(input())
+    if coins <= 0:
+        count += 1
+print(count)
 
-# summa = int(input("Sum is: "))
-# product = int(input("Product is: "))
-# for i in range(summa):
-#     for j in range(product):
-#         if summa == i + j and product == i * j:
-#             print(i, j)
+summa = int(input("Sum is: "))
+product = int(input("Product is: "))
+for i in range(summa):
+    for j in range(product):
+        if summa == i + j and product == i * j:
+            print(i, j)
 
+n = int(input())
+i = 0
+while 2 ** i <= n:
+    print(2 ** i)
+    i += 1
