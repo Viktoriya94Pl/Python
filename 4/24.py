809ef863ea98817f2df47a1455f2f2f28c0718bb
Vika Platonova <vplatonova_1994@mail.ru>
2023-06-28 22:54:28 +0300

    Homework 4.1

--- 4/24.py
+++ 4/24.py
@@ -1,9 +1,9 @@
-bushes = int(input('Введите колличество кустов на грядке: '))
+bushes = int(input('Введите количество кустов на грядке: '))
 my_list = []
 new_list = []
 for _ in range(bushes):
-    number = (int(input('Введите колличество ягод на кустах: ')))
+    number = (int(input('Введите количество ягод на кустах: ')))
     my_list.append(number)
 for elem in range(len(my_list)):
     new_list.append(my_list[elem - 1] + my_list[(elem + 1) % len(my_list)] + my_list[elem])
-print("Самое большое колличество ягод равно:", max(new_list))
+print("Самое большое количество ягод равно:", max(new_list))
